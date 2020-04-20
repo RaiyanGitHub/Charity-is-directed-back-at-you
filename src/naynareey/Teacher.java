64a00ffcb61e471f07892e34f6be5c9e9c3b9ce9
RaiyanGitHub <raiyan.ahmad.saadi1@gmail.com>
2020-04-20 21:16:59 +0600

    Complete Teacher. but not tested any of them in main.

---
+++ src/naynareey/Teacher.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package naynareey;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author سعدي
+ */
+public class Teacher extends Person {
+    private int numCourses = 0;
+    private ArrayList<String> courses = new ArrayList<String>(20);
+    Teacher(String name, String address){
+        super(name,address);
+    }
+    
+    public String toString(){
+        String cours = "";
+        for(String x : courses){
+            cours += x+"\t";
+        }
+        return super.toString()
+                +"\n Number of Courses: "+numCourses
+                +"\n Courses: "+cours;
+    
+    }//to String
+    
+    public boolean addCourse(String course){
+        
+        if(this.courses.indexOf(course) == -1){//this method returns -1 if the argument object does not exist in the collection
+            this.courses.add(course);
+            return true;
+        }
+        
+        return false;
+    }//add Course
+    
+    public boolean removeCourse(String course){    
+        return this.courses.remove(course);
+        
+    }//remove course
+    
+    
+    
+    
+}
