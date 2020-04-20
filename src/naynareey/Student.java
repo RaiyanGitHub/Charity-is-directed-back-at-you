69f1ba397aa0f6394406b75ac3581744be851fdf
RaiyanGitHub <raiyan.ahmad.saadi1@gmail.com>
2020-04-20 23:32:39 +0600

    tested Teacher. Works. HOOORRAAH!

--- src/naynareey/Student.java
+++ src/naynareey/Student.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package naynareey;
 
@@ -33,7 +28,7 @@ public class Student extends Person {
     public void setGrades(ArrayList<Double> grades) {
         this.grades = grades;
     }
-    
+    //Code  above are getters and setters
     Student(String name, String address){
         super(name,address);
     }
